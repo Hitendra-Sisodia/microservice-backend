79b0fc6fc20565c93286255f6850a4822e723893
HITENDRA SISODIA <97343226+Hitendra-Sisodia@users.noreply.github.com>
2023-09-30 16:11:21 +0530

    Delete config directory

--- config/razerpay.js
+++
@@ -1,7 +0,0 @@
-const Razorpay = require("razorpay");
-require("dotenv").config();
-
-exports.instance = new Razorpay({
-    key_id: process.env.RAZERPAY_KEY,
-    key_secret: process.env.RAZERPAY_SECRET,
-});
